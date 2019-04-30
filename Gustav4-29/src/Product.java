c90bca6480849113dec0a26cb21ce9fe747128df
SowanSo <49200957+SowanSo@users.noreply.github.com>
2019-04-30 00:02:59 -0600

    Add files via upload

---
+++ Gustav4-29/src/Product.java
@@ -0,0 +1,86 @@
+public class Product
+{
+	private String product;
+	private float pricePerKGram;
+	private int quantity;
+	private int rating;
+	private float temp;
+	public String getProduct;
+	
+	public Product() {
+		super();
+		product = "null";
+		pricePerKGram = -1;
+		quantity = -1;
+		rating = -1;
+	}
+	
+
+	public Product(String product, float pricePerKGram, int quantity, String effects) {
+		super();
+		this.product = product;
+		this.pricePerKGram = pricePerKGram;
+		this.quantity = quantity;
+		this.rating = rating;
+	}
+
+	public String getProductName() {
+		return product;
+	}
+
+
+	public void setProductName (String product) {
+		this.product = product;
+	}
+
+
+	public float getPricePerKGram() {
+		return pricePerKGram;
+	}
+
+
+	public void setPricePerKGram(float pricePerKGram) {
+		this.pricePerKGram = pricePerKGram;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public int getRating() {
+		return rating;
+	}
+
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+
+	public float getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(float temp) {
+		this.temp = temp;
+	}
+
+
+	@Override
+	public String toString() {
+		return "\nProduct : " + product + ", \nPricePerGram : " + pricePerKGram + ", \nQuantity : " + quantity
+				+ "\n" + rating + ", " + temp;
+	}
+
+	
+	
+	
+}
